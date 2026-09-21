9f33c63bdcaf4a4e0ebc68948c0896a82b256436
J <jirigoyen@gmail.com>
2026-09-21 13:45:14 -0700

    Audit fixes (real upload): day-scoped state, no auto-start away from trailhead, conservative cutoffs, Viviane wording, GPS survives dropouts; electrolyte wording

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Enchantments planner - offline cache
-const BUILD='2026-09-21 13:25';
+const BUILD='2026-09-21 13:45';
 const CACHE='ench-'+BUILD;
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
