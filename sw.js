c1bc97a5083476ba4808c2d98028b5ca7035195e
J <jirigoyen@gmail.com>
2026-09-21 19:47:00 -0700

    Add collapsible per-milestone photo widget (no photos wired up yet)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Enchantments planner - offline cache
-const BUILD='2026-09-21 20:40';
+const BUILD='2026-09-21 21:05';
 const CACHE='ench-'+BUILD;
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
